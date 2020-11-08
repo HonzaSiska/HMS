b657f4122be1ae1b70447babec901ff02a4e0eb3
Siskajan <siskajan@hotmail.com>
2020-11-08 22:21:18 +0100

    change of background on Byty page

---
+++ System/Resource/js/slides.js
@@ -0,0 +1,28 @@
+
+            
+    $('.pictureSlide').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        easing: 'swing',
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+            }
+        }
+        ]
+    });
